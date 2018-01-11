b90176a82a5bcb4a147ff1c09edf0b94aefa1dd2
JorgeGamba <jorgegamba@gmail.com>
2018-01-11 01:52:33 -0500

    Gathers everything

---
+++ src/Webshop/App_Data/Creates the Products table.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [dbo].[Products] (
+    [Id]          INT             IDENTITY (1, 1) NOT NULL,
+    [Number]      INT             NOT NULL,
+    [Title]       NVARCHAR (MAX)  NOT NULL,
+    [Price]       DECIMAL (18, 2) NOT NULL,
+    [Description] NVARCHAR (MAX)  NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+
+GO
+CREATE UNIQUE NONCLUSTERED INDEX [IX_Products_Number]
+    ON [dbo].[Products]([Number] ASC);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_Products_Price]
+    ON [dbo].[Products]([Price] ASC);
